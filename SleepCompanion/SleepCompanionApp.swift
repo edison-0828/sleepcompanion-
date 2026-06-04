283f7a2a01a651f50ccc99e462a2132c7e10755a
edison <ganhui983@gmail.com>
2026-06-04 23:37:58 +0800

    Initial Commit

---
+++ SleepCompanion/SleepCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepCompanionApp.swift
+//  SleepCompanion
+//
+//  Created by nicolas on 2026/6/4.
+//
+
+import SwiftUI
+
+@main
+struct SleepCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SleepCompanionTests/SleepCompanionTests.swift
@@ -0,0 +1,18 @@
+//
+//  SleepCompanionTests.swift
+//  SleepCompanionTests
+//
+//  Created by nicolas on 2026/6/4.
+//
+
+import Testing
+
+struct SleepCompanionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
